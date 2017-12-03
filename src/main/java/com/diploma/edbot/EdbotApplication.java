37c0685ba8ffd75d7ad8655e21029e583e1061de
tkozak <taraskozak614@gmail.com>
2017-12-03 19:31:08 +0200

    Initial commit

---
+++ src/main/java/com/diploma/edbot/EdbotApplication.java
@@ -0,0 +1,12 @@
+package com.diploma.edbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdbotApplication.class, args);
+	}
+}
